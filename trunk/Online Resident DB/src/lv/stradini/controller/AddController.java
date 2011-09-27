45c3337e3c293b56b8e9a26bd5f00c8aa6fb25a4
panser <panser@72454975-ab8e-4399-8325-2f4f4d11b9c1>
2011-09-27 12:39:48 +0000

    Implemented heart creation

--- trunk/Online Resident DB/src/lv/stradini/controller/AddController.java
+++ trunk/Online Resident DB/src/lv/stradini/controller/AddController.java
@@ -3,6 +3,7 @@ package lv.stradini.controller;
 
 
 import lv.stradini.constants.Constants;
+import lv.stradini.domain.Heart;
 import lv.stradini.domain.Resident;
 import lv.stradini.interfaces.service.ResidentService;
 
@@ -11,11 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value={"/resident/addResident.htm", "/resident/updateResident.htm"})
+@RequestMapping(value={"/resident/addResident.htm", "/resident/addHeart.htm", "/resident/updateResident.htm"}) //TODO: remove update resident mapping
 public class AddController {
 
 	private static Logger log = Logger.getLogger(AddController.class);
@@ -23,7 +23,7 @@ public class AddController {
 	private ResidentService residentService;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView showNewForm() {
+	public ModelAndView showNewResidentForm() {
 		log.info("AddNewResController: in show()");
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("resident", new Resident());
@@ -33,13 +33,28 @@ public class AddController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, params={"updateResidentID"})
-	public ModelAndView showUpdateForm(@RequestParam("updateResidentID") long updateResidentID) {
+	public ModelAndView showUpdateResidentForm(long updateResidentID) {
 		log.info("AddNewResController: in showUpdate()");
 		ModelAndView mav = new ModelAndView();
 		Resident resident = residentService.findResidentByID(updateResidentID);
 		mav.addObject("resident", resident);
 		mav.addObject("actionType", Constants.ACTION_TYPE_UPDATE);
 		mav.setViewName("add/addResident");
+		return mav;
+	}
+	
+	@RequestMapping(value="addHeart.htm", method = RequestMethod.POST)
+	public ModelAndView showAddHeartForm(long residentFK) {
+		log.info("AddNewResController: in showAddHeartForm()");
+		
+		
+		ModelAndView mav = new ModelAndView();
+		
+		Heart heart = new Heart();
+		heart.setResidentFK(residentFK);
+		mav.addObject("heart", heart);
+		mav.addObject("actionType", Constants.ACTION_TYPE_NEW);
+		mav.setViewName("add/addHeart");
 		return mav;
 	}
 }

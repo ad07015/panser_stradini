04767650ed6398232abe6223cfc640dbd87e44a5
panser <panser@72454975-ab8e-4399-8325-2f4f4d11b9c1>
2012-01-05 20:16:38 +0000

    Implemented Http tests of Events page

--- lab02-events/lab02-events-core/src/main/java/lv/lu/events/constants/GeneralConst.java
+++
@@ -1,6 +0,0 @@
-package lv.lu.events.constants;
-
-public final class GeneralConst {
-	
-	public final static String LOG_SEPARATOR = "-----------------------------------------------------------";
-}

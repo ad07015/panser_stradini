dc157aa9c9ad78e92677d28b5ac02d6771d2963e
panser <panser@72454975-ab8e-4399-8325-2f4f4d11b9c1>
2014-03-02 17:43:06 +0000

    Persistance code

--- meapPlay2ForJava/app/models/Product.java
+++ meapPlay2ForJava/app/models/Product.java
@@ -1,32 +1,47 @@
 package models;
 
+import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
 
-public class Product {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-    public interface All {}
+import play.db.ebean.Model;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+public class Product extends Model {
+
+	@Id
+	public Long id;
 
 	public Long ean;
 	public String name;
 	public String description;
 
+	@OneToMany(mappedBy = "product", cascade = ALL)
+	public List<StockItem> stockItems;
+
 	private static Set<Product> products;
 	static {
 		products = new HashSet<Product>();
 		products.add(new Product(1111111111111L, "Paperclips 1",
-			"Paperclips description 1"));
+				"Paperclips description 1"));
 		products.add(new Product(2222222222222L, "Paperclips 2",
-			"Paperclips description "));
+				"Paperclips description "));
 		products.add(new Product(3333333333333L, "Paperclips 3",
-			"Paperclips description 3"));
+				"Paperclips description 3"));
 		products.add(new Product(4444444444444L, "Paperclips 4",
-			"Paperclips description 4"));
+				"Paperclips description 4"));
 		products.add(new Product(5555555555555L, "Paperclips 5",
-			"Paperclips description 5"));
+				"Paperclips description 5"));
 	}
 
-	public Product() {}
+	public Product() {
+	}
 
 	public Product(Long ean, String name, String description) {
 		this.ean = ean;
@@ -63,6 +78,10 @@ public class Product {
 
 	public static void add(Product product) {
 		products.add(product);
+	}
+
+	public static Finder<Long, Product> find() {
+		return new Finder<Long, Product>(Long.class, Product.class);
 	}
 
 	public String toString() {

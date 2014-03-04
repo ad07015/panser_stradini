ea7bbf9fd3c504cc43d1df151172afd046f1ebc9
panser <panser@72454975-ab8e-4399-8325-2f4f4d11b9c1>
2014-03-04 14:13:24 +0000

    Advanced queries

--- meapPlay2ForJava/app/controllers/Application.java
+++ meapPlay2ForJava/app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.lang.StringBuilder;
 
 import com.avaje.ebean.Ebean;
 
@@ -15,36 +16,55 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-    	Warehouse warehouse = new Warehouse();
+/*    	Warehouse warehouse = new Warehouse();
     	warehouse.name = "My Warehouse";
 
-    	Product product = new Product();
-    	product.name = "Stainless steel paperclips, small, 1000pcs";
-    	product.ean = 1234L;
-    	product.description = "1000 blue paperclips.";
+        Product product = null;
+        StockItem item;
+        for (long i = 100L; i < 1500; i += 100L) {
+            product = new Product();
+            product.name = "Stainless steel paperclips, small, 600pcs";
+            product.ean = 1234L;
+            product.description = "1000 blue paperclips.";
 
-    	StockItem item = new StockItem();
-    	item.quantity = 15L;
-    	item.product = product;
+            item = new StockItem();
+            item.quantity = i;
+            item.product = product;
 
-    	item.warehouse = warehouse;
-    	warehouse.stockItems.add(item);
+            item.warehouse = warehouse;
+            warehouse.stockItems.add(item);
 
-    	List<String> output = new LinkedList<String>();
-    	output.add(String.format("My warehouse is called '%s'", warehouse));
-    	output.add(String.format("It contains %d items", warehouse.stockItems.size()));
-    	output.add(String.format("The first is: %s", warehouse.stockItems.get(0)));
+//            List<String> output = new LinkedList<String>();
+//            output.add(String.format("My warehouse is called '%s'", warehouse));
+//            output.add(String.format("It contains %d items", warehouse.stockItems.size()));
+//            output.add(String.format("The first is: %s", warehouse.stockItems.get(0)));
 
-    	product.save();
-    	item.save();
+        	product.save();
+        	item.save();
+            
+            Logger.info(String.format("Product name: %s", product.name));
+            Logger.info(String.format("StockItems: %d", product.stockItems.size()));
+        }*/        
 
-         Product product2 = Ebean.find(Product.class, 1L);
+        // Product product2 = Ebean.find(Product.class, 1L);
 //        Product product2 = Product.find().byId(1L);
-        Logger.info(String.format("Product name: %s", product.name));
-        Logger.info(String.format("StockItems: %d", product.stockItems.size()));
         
-        List<StockItem> items = StockItem.find().findList();
-        
+        List<StockItem> items;
+        // items = StockItem.find().findList();
+        items = StockItem.find()
+            .where()
+            .ge("quantity", 300)
+            .orderBy("quantity")
+            .setMaxRows(5)
+            .findList();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("StockItem list: ").append("\n");
+        for (StockItem temp : items) {
+            sb.append(temp.toString()).append("\n");
+        }
+        Logger.info(sb.toString());
+
 //        return ok(items);
 
         return ok(index.render("Your new application is ready."));
